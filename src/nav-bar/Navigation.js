2218e6e16bcf99cc3680fd5e823359cecb41916c
Singh, Richa <rrichaz@hotmail.com>
2024-02-08 14:50:45 -0600

    use bootstrap classes for hamburger

--- src/nav-bar/Navigation.js
+++ src/nav-bar/Navigation.js
@@ -1,28 +1,42 @@
 import './Navigation.css';
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
 import logo from './logo.png';
 
 const Navigation = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    return (       
-            <nav>
-              <img src={logo} width="70" height="50" className="px-2"alt="logo"/>
-              <h4>21st Century F1</h4>
-              {<div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
-              </div>  }
-              <ul className={menuOpen ? "open" : ""}>
-                <li>
-                  <NavLink to="/" className={"nav-link text-white nav-effect"}> Home</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/standings" className={"nav-link text-white nav-effect"}>Standings</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/results" className={"nav-link text-white nav-effect"}>GP Results</NavLink>
-                </li>
-              </ul>
+    // const [menuOpen, setMenuOpen] = useState(false);
+    return (    
+
+            <nav className='navbar navbar-expand-lg navbar-dark fixed-top bg-dark'>
+              <div class="container">
+                
+                    <img src={logo} width="30" height="30" class="d-inline-block align-top"
+                        alt="F1 Car Logo"/>
+                        <h4>21st Century F1</h4>
+          
+        
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="nav navbar-nav mr-auto text-center">
+                    <li>
+                      <NavLink to="/" className={"nav-link nav-effect"}> Home</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/standings" className={"nav-link text-white nav-effect"}>Standings</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/results" className={"nav-link text-white nav-effect"}>GP Results</NavLink>
+                    </li>
+                  </ul>
+                </div>
+                </div>
             </nav>
+
           );
 };
  
