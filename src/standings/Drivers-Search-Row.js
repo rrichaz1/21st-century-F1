3443e64f9685e7a5d3309a5a12457c4b631ef53c
Richa Singh <rrichaz@hotmail.com>
2024-03-06 22:25:59 -0600

    Fix image paths (#3)
    * Update Drivers-Search-Row.js for image path
    
    * Update Drivers-Search-Row.js
    
    * Remove nationality name

--- src/standings/Drivers-Search-Row.js
+++ src/standings/Drivers-Search-Row.js
@@ -1,15 +1,14 @@
 import "./Standings.css";
-// import Image from "react-bootstrap/Image";
+import Image from "react-bootstrap/Image";
 
 const DriversSearchRow = ({driverStanding}) => {
-    // const icon = `/21st-century-F1/public/icons/${driverStanding.Driver.nationality}.jpg`;
+    const icon = `/21st-century-F1/icons/${driverStanding.Driver.nationality}.png`;
 
     return ( 
         <tr>
             <td>{driverStanding.position}</td>
             <td>
-                {/* <Image src={icon} width="35" height="35"/> */}
-                {driverStanding.Driver.nationality}
+                { <Image src={icon} width="35" height="35"/> }
             </td>
             <td>{driverStanding.Driver.givenName + " " + driverStanding.Driver.familyName}</td>
             <td>{driverStanding.Constructors[0].name}</td>
